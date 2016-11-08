207d771e7b0f866c749535bb35c842665b892258
文怀宇 <wenhuaiyu@imobpay.com>
2016-11-08 17:17:41 +0800

    微信接口迁移-devbranch:1、用户注册TA接口提交，2、常量类提交

---
+++ src/main/java/com/imobpay/base/console/UserConsoleColumn.java
@@ -0,0 +1,237 @@
+/** 
+* 包名: package com.imobpay.base.console; <br/> 
+* 添加时间: 2016年11月8日 下午1:44:25 <br/> 
+*/
+package com.imobpay.base.console;
+
+/**
+ * 
+ * <pre>
+ * 【类型】: User_Console <br/> 
+ * 【作用】: 用户服务常量类. <br/>  
+ * 【时间】：2016年11月8日 下午1:47:49 <br/> 
+ * 【作者】：HuaiYu.Wen <br/> 
+ * </pre>
+ */
+public class UserConsoleColumn {
+    /******************搬迁微信服务常量字段开始*****************/
+    /** 返回空值 */
+    public static final String RETURNNULL               = "";
+    /** 交易码 */
+    public static final String JYM                      = "JYM";
+    /** 跳转URL */
+    public static final String RESPONSE_URL             = "RESPONSE_URL";
+    /** TA卡用户状态 1 正常 2冻结 */
+    public static final String TA_STATUS_1              = "1";
+    /** TA卡用户状态 1 正常 2冻结 */
+    public static final String TA_STATUS_2              = "2";
+    /** 用户状态 1 正常 2冻结 */
+    public static final String TA_CUS_STATUS_1          = "1";
+    /** 用户状态 1 正常 2冻结 */
+    public static final String TA_CUS_STATUS_2          = "2";
+    /** 手机号 */
+    public static final String MOBILENO                 = "MOBILENO";
+    /** 应用类型 */
+    public static final String APPUSER                  = "APPUSER";
+    /** 登录密码 */
+    public static final String PASSWORD                 = "PASSWORD";
+    /** 短信验证 */
+    public static final String MOBILEMAC                = "MOBILEMAC";
+    /** 短信验证 */
+    public static final String RS_MOBILEMAC             = "mobileMac";
+    /** TA卡号 */
+    public static final String TAACCOUNT                = "TAACCOUNT";
+    /** 店铺名称 */
+    public static final String SHOPNAME                 = "SHOPNAME";
+    /** 主营业务 */
+    public static final String MAINBUS                  = "MAINBUS";
+    /** 经营人姓名 */
+    public static final String USEFNAME                 = "USEFNAME";
+    /** 证件号码 */
+    public static final String CUSTOMERPID              = "CUSTOMERPID";
+    /** 客户号 */
+    public static final String CUSTOMERID               = "CUSTOMERID";
+    /** 省 */
+    public static final String PROVINCE                 = "PROVINCE";
+    /** 城市 */
+    public static final String CITY                     = "CITY";
+    /** 区 */
+    public static final String REGION                   = "REGION";
+    /** 地址 */
+    public static final String ADDRESS                  = "ADDRESS";
+    /** 卡号 */
+    public static final String CARDNO                   = "CARDNO";
+    /** 支行号 */
+    public static final String BANKBRANCHID             = "BANKBRANCHID";
+    /** 卡类型D借记卡 */
+    public static final String CARD_TYPE_D              = "D";
+    /** IP */
+    public static final String IP                       = "IP";
+    /** 经度 */
+    public static final String LONGITUDE                = "LONGITUDE";
+    /** 纬度 */
+    public static final String LATITUDE                 = "LATITUDE";
+    /** 身份证号码 */
+    public static final String IDNO                     = "IDNO";
+    /** 短信验证码的验证标识 */
+    public static final String CHECKTAG_0               = "0";
+    /** TA卡号 */
+    public static final String TA_ACCOUNT               = "TAACCOUNT";
+    /** 当前页 */
+    public static final String CURR_PAGE                = "CURRPAGE";
+    /** 来源 app或web */
+    public static final String FROMTYPE                 = "FROMTYPE";
+    /** 来源 app */
+    public static final String FROMAPP                  = "app";
+    /** 来源 web */
+    public static final String FROMWEB                  = "web";
+    /** 每页条数 */
+    public static final int    SIZE_PAGE                = 10;
+    /** 是否是最后一条 */
+    public static final String ISLAST                   = "isLast";
+    /** 返回结果 */
+    public static final String RESULTS                  = "results";
+    /** 返回结果 */
+    public static final String RESULT_BEAN              = "resultBean";
+    /** 返回结果 */
+    public static final String RS_LOCALDATE             = "localDate";
+    /** 返回结果 */
+    public static final String RS_LOCALTIME             = "localTime";
+    /** 返回结果 */
+    public static final String RS_ORDER_ID              = "orderId";
+    /** 返回结果 */
+    public static final String RS_PAYTYPE               = "payType";
+    /** 返回结果 */
+    public static final String RS_ACCOUNTNO             = "accountNo";
+    /** 返回结果 */
+    public static final String ACCOUNTNO                = "ACCOUNTNO";
+    /** 返回结果 */
+    public static final String RS_AMOUNT                = "amount";
+    /** 返回结果 */
+    public static final String RS_TAACCOUNT             = "taAccount";
+    /** 返回结果 */
+    public static final String RS_FEE                   = "fee";
+    /** 返回结果 */
+    public static final String RS_FD_FEE                = "fdFee";
+    /** 返回结果 */
+    public static final String RS_LONGITUDE             = "longitude";
+    /** 返回结果 */
+    public static final String RS_LATITUDE              = "latitude";
+    /** 返回结果 */
+    public static final String RS_MSGTEXT               = "msgText";
+    /** 返回结果 */
+    public static final String RS_MSGCODE               = "msgCode";
+    /** 返回结果 */
+    public static final String RS_LOCALLOGNO            = "localLogno";
+    /** 返回结果 */
+    public static final String RS_MERCHANTID            = "merchantId";
+    /** 用户信息 */
+    /** 返回结果 */
+    public static final String MOBILE                   = "mobile";
+    /** 姓名 */
+    public static final String USERNAME                 = "userName";
+    /** 返回结果 */
+    public static final String RS_CUSTOMERPID           = "customerPid";
+    /** 返回结果 */
+    public static final String RS_SHOPNAME              = "shopName";
+    /** 返回结果 */
+    public static final String RS_MAIN_BUS              = "mainBus";
+    /** 返回结果 */
+    public static final String RS_PROVINCE              = "province";
+    /** 返回结果 */
+    public static final String RS_CITY                  = "city";
+    /** 返回结果 */
+    public static final String RS_REGION                = "region";
+    /** 返回结果 */
+    public static final String RS_ADDRESS               = "address";
+    /** 返回结果 */
+    public static final String RS_BRANCHID              = "branchid";
+    /** 返回结果 */
+    public static final String RS_STATUS                = "status";
+    /** 返回结果 */
+    public static final String RS_ACCOUNTNANME          = "accountName";
+    /** 银行ID */
+    public static final String RS_BANKID                = "bankId";
+    /** 二维码url */
+    public static final String RS_QRCODEURL             = "qrCodeUrl";
+    /** 返回结果 */
+    public static final String RS_CUSTOMERREGDATE       = "customerRegDate";
+    /** 返回结果 */
+    public static final String UPD_TYPE                 = "UPDTYPE";
+    /** 返回结果 */
+    public static final String CONTENT                  = "CONTENT";
+    /** 结算卡修改 */
+    public static final String UPD_TYPE_1               = "1";
+    /** 密码修改 */
+    public static final String UPD_TYPE_2               = "2";
+    /** 商户信息修改 */
+    public static final String UPD_TYPE_3               = "3";
+    /** 用户客户端密码 */
+    public static final String PAYPASSWORD              = "PAYPASSWORD";
+    /** 用户客户端密码 */
+    public static final String RS_PAYPASSWORD           = "payPassword";
+    /** 用户客户端密码 */
+    public static final String RS_NEW_PAYPASSWORD       = "newPassword";
+    /** 归属人姓名 */
+    public static final String ACCOUNTNAME              = "ACCOUNTNAME";
+    /** 微信注册 */
+    public static final String PARAME_REG               = "WxReg";
+    /** 微信支付 */
+    public static final String PARAME_PAY               = "WxPay";
+    /** 微信支付 APP跳转 */
+    public static final String PARAME_PAY_APP           = "WxPayApp";
+    /** 微信冻结 */
+    public static final String PARAME_DJ                = "WxDj";
+    /** 微信用户openid */
+    public static final String OPENID                   = "OPENID";
+    /** 微信用户openid */
+    public static final String OPENID_X                 = "openId";
+    /** 微信APPID */
+    public static final String APPID                    = "APPID";
+    /** 微信APPID */
+    public static final String APPIDVAL                 = "APPIDVAL";
+    /** 微信APPID */
+    public static final String TICKET                   = "TICKET";
+    /** 微信APPID */
+    public static final String TICKETVAL                = "TICKETVAL";
+    /** 日志进程号 */
+    public static final String LOGKEYALL                = "LOGKEYALL";
+    /** 授权码 */
+    public static final String ACCESSTOKEN              = "ACCESSTOKEN";
+    /** 图片存取地址 */
+    public static final String PICURL                   = "picURL";
+    /** 文件路径 */
+    public static final String FILEDIR                  = "FileDir";
+    /** 获取公钥 */
+    public static final String DES_KEY                  = "3DES_KEY";
+    /** 获取盐值 */
+    public static final String DES_YAN                  = "3DES_YAN";
+    /** 微信公众号 */
+    public static final String PUB_ACCOUNT              = "pubAccount";
+    /** 机构号 */
+    public static final String BRANCHID                 = "branchId";
+    /** 问候语 */
+    public static final String FIRSTVAL                 = "firstVal";
+    /** 推送内容 */
+    public static final String KEYWORDVAL               = "keyword{0}Val";
+    /** 备注 */
+    public static final String REMARKVAL                = "remarkVal";
+    /** 交易推送类型 */
+    public static final String WX_MSG_TRADE_TYPE        = "wxMsgTradeType";
+    /** 消息个数 */
+    public static final String WX_MSG_TEM_CONTENT_COUNT = "wxMsgTemContentCount";
+    /** 推送详情URL */
+    public static final String PUSH_URL                 = "pushUrl";
+    /** 用户ID */
+    public static final String TOUSER                   = "touser";
+    /** 模板ID */
+    public static final String TEMPLATE_ID              = "template_id";
+    /** 发送数据 */
+    public static final String DATA                     = "data";
+    /** 详情url */
+    public static final String URL                      = "url";
+    /** 需要特殊处理的appuser和feeid(第一版只用于瑞钱包VIP)参数配置KEY */
+    public static final String VIPAPPUSER               = "VIPAPPUSER";
+    /******************搬迁微信服务常量字段结束*****************/
+
+}
---
+++ src/main/java/com/imobpay/base/services/impl/ServicesUserRegisterTA.java
@@ -0,0 +1,279 @@
+
+ /** 
+ * 包名: package com.imobpay.base.services.impl; <br/> 
+ * 添加时间: 2016年11月8日 上午11:11:19 <br/> 
+ */
+package com.imobpay.base.services.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alibaba.fastjson.JSONObject;
+import com.imobpay.base.config.PropertiesConfig;
+import com.imobpay.base.config.SendEncrypt;
+import com.imobpay.base.console.Console_ErrCode;
+import com.imobpay.base.console.UserConsoleColumn;
+import com.imobpay.base.dao.CsCardbinDao;
+import com.imobpay.base.dao.MobilepaycheckcodeDao;
+import com.imobpay.base.dao.PrepClientVersionDao;
+import com.imobpay.base.dao.TbvActivityLotteryDao;
+import com.imobpay.base.dao.TbvBindcardDao;
+import com.imobpay.base.dao.TbvCardDao;
+import com.imobpay.base.dao.TbvCustomerDao;
+import com.imobpay.base.dao.TbvSecurityDao;
+import com.imobpay.base.entity.CsBinBankInfo;
+import com.imobpay.base.entity.CsCardbin;
+import com.imobpay.base.entity.Mobilepaycheckcode;
+import com.imobpay.base.entity.PrepClientVersion;
+import com.imobpay.base.entity.TbvActivityLottery;
+import com.imobpay.base.entity.TbvBindcard;
+import com.imobpay.base.entity.TbvCard;
+import com.imobpay.base.entity.TbvCustomer;
+import com.imobpay.base.entity.TbvSecurity;
+import com.imobpay.base.exception.QTException;
+import com.imobpay.base.iface.BusinessInterface;
+import com.imobpay.base.log.LogPay;
+import com.imobpay.base.util.DateUtil;
+import com.imobpay.base.util.EmptyChecker;
+import com.imobpay.base.util.Tools;
+
+/**
+ * 
+ * <pre>
+ * 【类型】: ServicesUserRegisterTA <br/> 
+ * 【作用】: 用户注册 (TA). <br/>  
+ * 【时间】：2016年11月8日 上午11:11:24 <br/> 
+ * 【作者】：HuaiYu.Wen <br/> 
+ * </pre>
+ */
+public class ServicesUserRegisterTA implements BusinessInterface {
+
+    /** 卡类型列表 */
+    public static Map<String, String> cType = new HashMap<String, String>();
+    static {
+        /** 01:借记卡 , 02:预付卡 , 03: 信用卡 */
+        cType.put("01", "D");
+        cType.put("02", "P");
+        cType.put("03", "C");
+    }
+
+    /**
+     * 中奖抽取表
+     */
+    @Resource
+    TbvActivityLotteryDao<TbvActivityLottery> tbvActiveLotteryDao;
+    /**
+     * 客户表
+     */
+    @Resource
+    TbvCustomerDao<TbvCustomer>               tbvCusDao;
+
+    /**
+     * 卡bin表
+     */
+    @Resource
+    CsCardbinDao<CsCardbin>                   csCardBinDao;
+    /**
+     * 绑定卡表
+     */
+    @Resource
+    TbvBindcardDao<TbvBindcard>               tbvBindCardDao;
+    /**
+     * 密码表
+     */
+    @Resource
+    TbvSecurityDao<TbvSecurity>               tbvSecur;
+    /**
+     * 客户端版本表
+     */
+    @Resource
+    PrepClientVersionDao<PrepClientVersion>   clientDao;
+    /**
+     * TA卡
+     */
+    @Resource
+    TbvCardDao<TbvCard>                       tbvCardDao;
+    /**
+     * 短信验证码表
+     */
+    @Resource
+    MobilepaycheckcodeDao<Mobilepaycheckcode> mobCheckDao;
+    /***
+     * 
+     */
+    @Resource
+    ApplicationContext                        applicationContext;
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public String execute(String json) throws QTException {
+
+        JSONObject reqJson = JSONObject.parseObject(json);
+        EmptyChecker.checkEmpty(reqJson, UserConsoleColumn.MOBILENO, UserConsoleColumn.APPUSER, UserConsoleColumn.PASSWORD, UserConsoleColumn.TAACCOUNT, UserConsoleColumn.PASSWORD,
+                UserConsoleColumn.SHOPNAME, UserConsoleColumn.USEFNAME, UserConsoleColumn.CUSTOMERPID, UserConsoleColumn.MAINBUS, UserConsoleColumn.PROVINCE, UserConsoleColumn.CITY,
+                UserConsoleColumn.REGION, UserConsoleColumn.ADDRESS, UserConsoleColumn.CARDNO, UserConsoleColumn.OPENID);
+        // 判断手机号是否注册 或者 TA卡是否已经使用
+        String mobile = reqJson.getString(UserConsoleColumn.MOBILENO);
+        String appuser = reqJson.getString(UserConsoleColumn.APPUSER);
+        String passwd = reqJson.getString(UserConsoleColumn.PASSWORD);
+        String taAccountMac = reqJson.getString(UserConsoleColumn.TAACCOUNT);
+        String cardNo = reqJson.getString(UserConsoleColumn.CARDNO);
+        String userName = reqJson.getString(UserConsoleColumn.USEFNAME);
+        String pid = reqJson.getString(UserConsoleColumn.CUSTOMERPID);
+        String shopName = reqJson.getString(UserConsoleColumn.SHOPNAME);
+        String mainBus = reqJson.getString(UserConsoleColumn.MAINBUS);
+        String province = reqJson.getString(UserConsoleColumn.PROVINCE);
+        String city = reqJson.getString(UserConsoleColumn.CITY);
+        String region = reqJson.getString(UserConsoleColumn.REGION);
+        String address = reqJson.getString(UserConsoleColumn.ADDRESS);
+        String ip = reqJson.getString(UserConsoleColumn.IP);
+        String longitude = reqJson.getString(UserConsoleColumn.LONGITUDE);
+        String latitude = reqJson.getString(UserConsoleColumn.LATITUDE);
+
+        String openid = reqJson.getString(UserConsoleColumn.OPENID);
+        // 优先判断TA卡号是否存在
+        TbvCard tcard = new TbvCard();
+        tcard.setTaaccountmac(taAccountMac);
+        TbvCard taCard = tbvCardDao.selectById(tcard);
+        if (EmptyChecker.isEmpty(taCard)) {
+            throw new QTException(Console_ErrCode.URL_NOEXITISCODE, Console_ErrCode.URL_NOEXITISDESC);
+        }
+        /** 判断TA卡是否已绑定 */
+        if ("1".equalsIgnoreCase(taCard.getBindflag())) {
+            throw new QTException(Console_ErrCode.NOTE_BINDEDCODE, Console_ErrCode.NOTE_BINDEDDESC);
+        }
+        // 获取ta卡号
+        String taAccount = taCard.getTaaccount();
+
+        TbvCustomer t2 = new TbvCustomer();
+        t2.setTaaccount(taAccount);
+        TbvCustomer selectByIdTa = tbvCusDao.selectById(t2);
+        if (EmptyChecker.isNotEmpty(selectByIdTa)) {
+            throw new QTException(Console_ErrCode.NOTE_REGISTEREDCODE, Console_ErrCode.NOTE_REGISTEREDDESC);
+        }
+        // 判断当前卡是否为借记卡
+        CsBinBankInfo cb = new CsBinBankInfo();
+        cb.setCardno(cardNo);
+        cb.setCardleng(cardNo.length() + "");
+        CsBinBankInfo selectByCardLen = csCardBinDao.selectByCardLen(cb);
+        if (EmptyChecker.isEmpty(selectByCardLen)) {
+            throw new QTException(Console_ErrCode.NOTE_BANKINFODCODE, Console_ErrCode.NOTE_BANKINFODCODE);
+        }
+        String cardType = getCType(selectByCardLen.getCardtype());
+        if (!UserConsoleColumn.CARD_TYPE_D.equals(cardType)) {
+            throw new QTException(Console_ErrCode.NOTE_BINDDEBITCODE, Console_ErrCode.NOTE_BINDDEBITDESC);
+        }
+        // 验证短信验证码
+        PrepClientVersion pcv = new PrepClientVersion();
+        pcv.setAppuser(appuser);
+        PrepClientVersion selectPcv = clientDao.selectById(pcv);
+        if (EmptyChecker.isEmpty(selectPcv)) {
+            throw new QTException(Console_ErrCode.NOTE_NOBRANCHIDCODE, Console_ErrCode.NOTE_NOBRANCHIDDESC);
+        }
+        String tid = tbvCusDao.selectByTid();
+        LogPay.info("生成是序列ID:" + tid);
+        // 添加用户密码表
+        /** 发送加密机 获取加密密码 */
+        PropertiesConfig config = (PropertiesConfig) applicationContext.getBean("propertiesConfig");
+        String newPwd = SendEncrypt.send(mobile, passwd, config);
+
+        // 判断用户是否注册 放到这里的目的是为了防止程序运行太长 用户注册出现系统繁忙
+        TbvCustomer t = new TbvCustomer();
+        t.setMobile(mobile);
+        TbvCustomer selectByIdMob = tbvCusDao.selectById(t);
+        if (EmptyChecker.isNotEmpty(selectByIdMob)) {
+            throw new QTException(Console_ErrCode.NOTE_MOBILENOYETCODE, Console_ErrCode.NOTE_MOBILENOYETDESC);
+        }
+
+        TbvSecurity tsu = new TbvSecurity();
+        tsu.setTimestamp(DateUtil.getCurrDate(DateUtil.YYYYMMDDHHMMSS));
+        tsu.setTid(tid);
+        tsu.setPaypassword(newPwd);
+        tsu.setPassworderrcnt(0);
+        tbvSecur.insert(tsu);
+
+        // 添加用户信息
+        TbvCustomer tc = new TbvCustomer();
+        tc.setTid(tid);
+        tc.setMobile(mobile);
+        tc.setUsername(userName);
+        tc.setCustomerpid(pid);
+        tc.setShopname(shopName);
+        tc.setMainBus(mainBus);
+        tc.setProvince(province);
+        tc.setCity(city);
+        tc.setRegion(region);
+        tc.setAddress(address);
+        tc.setIp(ip);
+        tc.setLongitude(longitude);
+        tc.setLatitude(latitude);
+        tc.setBranchid(taCard.getBranchid());
+        tc.setTaaccount(taAccount);
+        tc.setStatus("1");
+        tc.setCustomerregdate(DateUtil.getCurrDate(DateUtil.YYYYMMDDHHMMSS));
+        tc.setOpenid(openid);
+        tbvCusDao.insert(tc);
+        // 添加绑定银行卡数据
+        TbvBindcard tbc = new TbvBindcard();
+        tbc.setId(Tools.getOnlyPK());
+        tbc.setTid(tid);
+        tbc.setAccountno(cardNo);
+        tbc.setAccountname(userName);
+        tbc.setBankid(selectByCardLen.getBankid());
+        tbc.setBankname(selectByCardLen.getBankname());
+        tbc.setBankbranchid(selectByCardLen.getBankbranchid());
+        tbc.setBankbranchname(selectByCardLen.getBankbranchname());
+        tbc.setProvinceid(selectByCardLen.getProvinceid());
+        tbc.setBankprovince(selectByCardLen.getProvincename());
+        tbc.setCityid(selectByCardLen.getCityid());
+        tbc.setBankcity(selectByCardLen.getCityname());
+        tbc.setBegindate(DateUtil.getCurrDate(DateUtil.YYYYMMDDHHMMSS));
+        tbc.setEnddate(DateUtil.getCurrDate(DateUtil.YYYYMMDDHHMMSS));
+        tbc.setBranchid(taCard.getBranchid());
+        tbc.setIdno(pid);
+        tbvBindCardDao.insert(tbc);
+        // 修改TA绑定卡的状态
+        tcard.setBindflag("1");
+        tcard.setTaaccount(taAccount);
+        tbvCardDao.update(tcard);
+        // 更新中奖纪录表里的TA卡状态
+        TbvActivityLottery tLottery = new TbvActivityLottery();
+        tLottery.setId(taAccount);
+        TbvActivityLottery selectById = tbvActiveLotteryDao.selectById(tLottery);
+        if (EmptyChecker.isNotEmpty(selectById)) {
+            LogPay.info("此TA卡为活动卡，同步更新活动表TA卡状态:" + taAccount);
+            TbvActivityLottery tbvActiveLottery = new TbvActivityLottery();
+            tbvActiveLottery.setId(taAccount);
+            tbvActiveLottery.setStatus("2");
+            tbvActiveLotteryDao.updateCodeLottery(tbvActiveLottery);
+        }
+
+        return UserConsoleColumn.RETURNNULL;
+    }
+    
+    /**
+     * 【方法名】    : getCType. <br/> 
+     * 【作用】: 获取卡类型.<br/> 
+     * 【作者】: HuaiYu.Wen .<br/>
+     * 【时间】： 2016年11月8日 下午2:25:08 .<br/>
+     * 【参数】： .<br/>
+     * @param key 
+     * @return .<br/>
+     * <p>
+     * 修改记录.<br/>
+     * 修改人: HuaiYu.Wen 修改描述：创建新新件 .<br/>
+     * <p/>
+     */
+    public static String getCType(String key) {
+        if (EmptyChecker.isEmpty(cType.get(key))) {
+            return key + "";
+        }
+        return (String) cType.get(key);
+    }
+}
+
